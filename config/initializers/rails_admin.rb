cdf92ac828d0348b7955d820915a96b9a90195f3
jimmy <solo123@21cn.com>
2016-10-17 00:20:42 +0800

    zh

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,20 +26,48 @@ RailsAdmin.config do |config|
 
   config.main_app_name = Proc.new { |controller| [ "PooulGate", "管理后台 - #{controller.params[:action].try(:titleize)}" ] }
   I18n.default_locale = :"zh-CN"
+  config.navigation_static_links = {
+    '临时后台' => 'http://b.pooulcloud.cn'
+  }
+  config.excluded_models = ["KaifuGateway", "KaifuQuery", "KaifuResult", "KaifuSignin"]
 
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
     new
     export
-    bulk_delete
+    #bulk_delete
     show
-    edit
-    delete
+    edit do
+      only ['AppConfig', 'Client']
+    end
+    #delete
     show_in_app
+
 
     ## With an audit adapter, you can add:
     # history_index
     # history_show
   end
+  config.model 'Client' do
+    navigation_label '系统管理'
+    label '商户管理'
+  end
+  config.model 'User' do
+    navigation_label '系统管理'
+    label '管理用户'
+  end
+  config.model 'AppConfig' do
+    navigation_label '系统管理'
+    label '系统配置'
+    #list do
+    #  field :name
+    #  field :created_at
+    #end
+  end
+  config.model 'BizError' do
+    navigation_label '系统管理'
+    label '错误信息'
+  end
+
 end
